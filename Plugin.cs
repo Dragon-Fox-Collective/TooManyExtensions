03a9e9f849884069913e4e151bdfd43dc6c1820a
Jady Wright <iamjwright159@gmail.com>
2024-01-26 11:08:51 -0500

    Add plugin files

---
+++ Plugin.cs
@@ -0,0 +1,13 @@
+#if TOOLS
+using BoundInputSystem.Actions;
+using BoundInputSystem.Bindings;
+using Godot;
+
+namespace TooManyExtensions;
+
+[Tool]
+public partial class Plugin : EditorPlugin
+{
+	
+}
+#endif

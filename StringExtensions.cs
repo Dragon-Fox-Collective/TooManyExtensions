e47a24c40391009dedf706bc5c32bf62192bb291
Jady Wright <iamjwright159@gmail.com>
2024-04-29 13:08:54 -0400

    Add string extensions for splitting into a tuple and checking if it's empty

---
+++ StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+namespace TooManyExtensions;
+
+public static class StringExtensions
+{
+	public static (string, string) SplitOnce(this string str, char separator)
+	{
+		int index = str.IndexOf(separator);
+		return index == -1 ? (str, string.Empty) : (str[..index], str[(index + 1)..]);
+	}
+	
+	public static bool IsEmpty(this string? str)
+	{
+		return string.IsNullOrEmpty(str);
+	}
+}

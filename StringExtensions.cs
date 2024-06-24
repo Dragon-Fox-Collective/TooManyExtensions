7d60f32bc7618ac41b15a6ffb82273a3a185e497
Jady Wright <iamjwright159@gmail.com>
2024-06-24 19:38:13 -0400

    Default SplitLast to second tuple value

--- StringExtensions.cs
+++ StringExtensions.cs
@@ -12,7 +12,7 @@ public static class StringExtensions
 	public static (string, string) SplitLast(this string str, char separator)
 	{
 		int index = str.LastIndexOf(separator);
-		return index == -1 ? (str, string.Empty) : (str[..index], str[(index + 1)..]);
+		return index == -1 ? (string.Empty, str) : (str[..index], str[(index + 1)..]);
 	}
 	
 	public static bool IsEmpty(this string? str)
